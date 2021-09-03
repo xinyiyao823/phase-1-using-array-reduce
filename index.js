fce6d1c6b584575409744c96e119abf3a2cec163
+ <+>
2021-09-02 21:55:46 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 // Code your solution here
 
 const totalBatteries = batteryBatches.reduce((previousValue, currentValue) => previousValue + currentValue, 0)
-console.log(totalBatteries)
+totalBatteries;
